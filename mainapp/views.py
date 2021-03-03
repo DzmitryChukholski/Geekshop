53bb9d2b307e603594d29d3bbcd1044e50cf7f74
Dzmitry Chukholski <hakuteken@gmail.com>
2021-03-03 15:59:47 +0300

    lesson 3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 import datetime
 
+from mainapp.models import Product, ProductCategory
+
 def index(request):
     context = {'title': 'GeekShop'}
     return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    context = {'title': 'GeekShop - Каталог', 'date_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+    context = {
+        'title': 'GeekShop - Каталог', 'date_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
     return render(request, 'mainapp/products.html', context)
