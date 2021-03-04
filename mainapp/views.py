d488f3764548a6ac67fbe3b83913be40e0d657e1
Dzmitry Chukholski <hakuteken@gmail.com>
2021-03-04 21:36:41 +0300

    urls

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,7 +7,7 @@ def index(request):
     context = {'title': 'GeekShop'}
     return render(request, 'mainapp/index.html', context)
 
-def products(request):
+def products(request, id=None):
     context = {
         'title': 'GeekShop - Каталог', 'date_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'products': Product.objects.all(),
